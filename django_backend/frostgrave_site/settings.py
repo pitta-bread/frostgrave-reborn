f624f04579c23a3141465ae21c465f027782af80
pbeards <peterbeardsmore@gmail.com>
2025-06-07 17:14:50 +0100

    try again to deploy

--- django_backend/frostgrave_site/settings.py
+++ django_backend/frostgrave_site/settings.py
@@ -144,7 +144,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = f"{BASE_DIR}/../gcs_bucket/static/"
+STATIC_ROOT = f"{BASE_DIR}/../gcs_bucket/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
